bd226d4dda1a0846c9c6236ef499c5c2ad241196
Robin Murray <rmurray@Ophion.local>
2016-09-24 13:54:27 -0400

    Stripped out stuff from game.js

--- game.js
+++ game.js
@@ -16,18 +16,12 @@ function create() {
     player = game.add.sprite(100, 200, 'player');
     game.physics.arcade.enable(player);
     player.body.collideWorldBounds = true;
-    player.body.gravity.y = 500;
-    platforms = game.add.physicsGroup();
-    platforms.create(500, 150, 'platform');
-    platforms.create(-200, 300, 'platform');
-    platforms.create(400, 450, 'platform');
-    platforms.setAll('body.immovable', true);
+    player.body.gravity.y = 0;
     cursors = game.input.keyboard.createCursorKeys();
-    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    //jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
 
 function update () {
-    game.physics.arcade.collide(player, platforms);
     player.body.velocity.x = 0;
 
     if (cursors.left.isDown)
@@ -37,11 +31,6 @@ function update () {
     else if (cursors.right.isDown)
     {
         player.body.velocity.x = 250;
-    }
-
-    if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down))
-    {
-        player.body.velocity.y = -400;
     }
 }
 
