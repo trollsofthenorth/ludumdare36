08d4e8ee6001b16fc27672aa8f0be34db45b1976
Daniel de Sao Jose <danieldsj@gmail.com>
2016-09-24 18:36:35 -0400

    Fixed the lemming spritesheet and captured all animations.

--- game.js
+++ game.js
@@ -1,10 +1,17 @@
 
 function range(start, end) {
   var myArray = [];
-  for (var i = start; i <= end; i += 1) {
-    myArray.push(i);
+  if (start < end) {
+    for (var i = start; i <= end; i += 1) {
+      myArray.push(i);
+    }
+  } else if (start > end){
+    for (var i = start; i >= end; i -= 1) {
+      myArray.push(i);
+    }
   }
   return myArray;
+
 }
 
 
@@ -70,16 +77,25 @@ Lemmings.prototype = {
         this.collision.addToWorld();
 
         this.lemming = this.add.sprite(10,10,'lemming');
-        this.lemming.animations.add('walk',range(0,7), 10, true);
-        this.lemming.animations.add('shrug',range(8,15), 10, true);
-        this.lemming.animations.add('climb-over',range(16,24), 10, true);
-        this.lemming.animations.add('black1',range(24,29), 10, true);
-        this.lemming.animations.add('fall',range(30,33), 10, true);
-        this.lemming.animations.add('parachute',range(34,41), 10, true);
-        this.lemming.animations.add('black2',range(42,44), 10, true);
-        this.lemming.animations.add('block',range(45,58), 10, true);
-        this.lemming.animations.add('block',range(45,58), 10, true);
-        this.lemming.play('block');
+        this.lemming.alpha=0.5; // This makes the background transparent for the sprite.
+        this.lemming.animations.add('walker',range(0,7), 10, true);
+        this.lemming.animations.add('shrugger',range(8,15), 10, true);
+        this.lemming.animations.add('exiter',range(16,24), 10, true);
+        this.lemming.animations.add('black1',range(25,31), 10, true);
+        this.lemming.animations.add('faller',range(32,35), 10, true);
+        this.lemming.animations.add('parachuter',range(36,43), 10, true);
+        this.lemming.animations.add('black2',range(44,47), 10, true);
+        this.lemming.animations.add('blocker',range(48,63), 10, true);
+        this.lemming.animations.add('climber',range(64,71), 10, true);
+        this.lemming.animations.add('climber-over',range(72,79), 10, true);
+        this.lemming.animations.add('builder',range(80,95), 10, true);
+        this.lemming.animations.add('basher',range(96,127), 10, true);
+        this.lemming.animations.add('digger',range(128,135), 10, true);
+        this.lemming.animations.add('miner',range(136,159), 10, true);
+        this.lemming.animations.add('dying',range(160,191), 10, true);
+        this.lemming.animations.add('drowning',range(192,207), 10, true);
+        this.lemming.animations.add('tnt',range(208,223), 10, true);
+        this.lemming.play('tnt');
 
         this.player = this.add.sprite(160,330,'player');
         this.physics.arcade.enable(this.player);
