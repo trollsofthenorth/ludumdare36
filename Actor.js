32f6d39a0da77ce57628e8c5775c5f244872e195
Alan Castonguay <acastonguay@vmware.com>
2016-09-25 13:47:26 -0400

    refactored Actor to seperate file

--- Actor.js
+++ Actor.js
@@ -1,50 +1,61 @@
 
 /* State machine for a walking/falling/digging actor. */
 
-(function () {
-	'use strict';
-  var ActorState = {
-    walker : "walker"
-    shrugger : "shrugger"
-    exiter : "exiter"
-    faller : "faller"
-    parachuter : "parachuter"
-    blocker : "blocker"
-    climber : "climber"
-    climber-over : "climber-over"
-    builder : "builder"
-    basher : "basher"
-    digger : "digger"
-    miner : "miner"
-    dyer : "dyer"
-    drowner : "drowner"
-    exploder : "exploder"
-  }
+
+/* OMG WAT ALAN */
+
+CloudPlatform = function (game, x, y, key, group) {
+    if (typeof group === 'undefined') { group = game.world; }
+    Phaser.Sprite.call(this, game, x, y, key);
+    game.physics.arcade.enable(this);
+    this.anchor.x = 0.5;
+    this.body.customSeparateX = true;
+    this.body.customSeparateY = true;
+    this.body.allowGravity = false;
+    this.body.immovable = true;
+    this.playerLocked = false;
+    group.add(this);
+};
+CloudPlatform.prototype = Object.create(Phaser.Sprite.prototype);
+CloudPlatform.prototype.constructor = CloudPlatform;
 
 
-  Lemming = function (game, x, y) {
-      x = startx || 0;
-      y = starty || 0;
-      key = key || null;
-      frame = frame || null;
+var ActorState = {
+  walker : "walker",
+  shrugger : "shrugger",
+  exiter : "exiter",
+  faller : "faller",
+  parachuter : "parachuter",
+  blocker : "blocker",
+  climber : "climber",
+  climberover : "climberover",
+  builder : "builder",
+  basher : "basher",
+  digger : "digger",
+  miner : "miner",
+  dyer : "dyer",
+  drowner : "drowner",
+  exploder : "exploder"
+}
 
-      Phaser.Sprite.call(this, game, x, y);
 
-      state = ActorState.faller;
-      /**
-      * @property {number} type - The const type of this object.
-      * @readonly
-      */
+Lemming = function (game, x, y, key, group) {
+    x = x || 0;
+    y = y || 0;
+    key = key || null;
+    frame = null;
+    if (typeof group === 'undefined') { group = game.world; }
 
-      /**
-      * @property {number} physicsType - The const physics body type of this object.
-      * @readonly
-      */
-      PIXI.Sprite.call(this, Phaser.Cache.DEFAULT);
+    Phaser.Sprite.call(this, game, x, y, key, frame);
+    this.state = ActorState.faller;
+    game.physics.arcade.enable(this);
+    this.anchor.x = 0.5;
+    this.body.customSeparateX = true;
+    this.body.customSeparateY = true;
+    this.body.allowGravity = true;
+    group.add(this);
+};
+Lemming.prototype = Object.create(Phaser.Sprite.prototype);
+Lemming.prototype.constructor = Lemming;
 
-      Phaser.Component.Core.init.call(this, game, x, y, key, frame);
-
-  };
-  Phaser.Lemming.prototype = Object.create(Phaser.Sprite.prototype);
-  Phaser.Lemming.prototype.constructor = Lemming;
-}();
+/* END OMG WAT ALAN */
--- game.js
+++ game.js
@@ -15,63 +15,6 @@ function range(start, end) {
 
 }
 
-/* OMG WAT ALAN */
-
-CloudPlatform = function (game, x, y, key, group) {
-    if (typeof group === 'undefined') { group = game.world; }
-    Phaser.Sprite.call(this, game, x, y, key);
-    game.physics.arcade.enable(this);
-    this.anchor.x = 0.5;
-    this.body.customSeparateX = true;
-    this.body.customSeparateY = true;
-    this.body.allowGravity = false;
-    this.body.immovable = true;
-    this.playerLocked = false;
-    group.add(this);
-};
-CloudPlatform.prototype = Object.create(Phaser.Sprite.prototype);
-CloudPlatform.prototype.constructor = CloudPlatform;
-
-
-var ActorState = {
-  walker : "walker",
-  shrugger : "shrugger",
-  exiter : "exiter",
-  faller : "faller",
-  parachuter : "parachuter",
-  blocker : "blocker",
-  climber : "climber",
-  climberover : "climberover",
-  builder : "builder",
-  basher : "basher",
-  digger : "digger",
-  miner : "miner",
-  dyer : "dyer",
-  drowner : "drowner",
-  exploder : "exploder"
-}
-
-
-Lemming = function (game, x, y, key, group) {
-    x = x || 0;
-    y = y || 0;
-    key = key || null;
-    frame = null;
-    if (typeof group === 'undefined') { group = game.world; }
-
-    Phaser.Sprite.call(this, game, x, y, key, frame);
-    this.state = ActorState.faller;
-    game.physics.arcade.enable(this);
-    this.anchor.x = 0.5;
-    this.body.customSeparateX = true;
-    this.body.customSeparateY = true;
-    this.body.allowGravity = true;
-    group.add(this);
-};
-Lemming.prototype = Object.create(Phaser.Sprite.prototype);
-Lemming.prototype.constructor = Lemming;
-
-/* END OMG WAT ALAN */
 
 var game = new Phaser.Game(320, 240, Phaser.CANVAS, 'startingstate');
 
@@ -123,6 +66,7 @@ Lemmings.prototype = {
 
         // Adding the spritesheet for a lemming.
         this.load.spritesheet('lemming', 'assets/spritesheet-lemming.png', 20, 20);
+        this.load.script('Actor', 'Actor.js');
 
     },
 
